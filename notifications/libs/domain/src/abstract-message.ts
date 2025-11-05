851d004983f1d3d9435c82ddbcd2c0176a1ed623
CMirianG <mc2021071083@virtual.upt.pe>
2025-11-05 05:59:47 -0500

    delete

--- notifications/libs/domain/src/abstract-message.ts
+++
@@ -1,6 +0,0 @@
-import { IMessageSender } from "./imessage-sender.interface";
-
-export abstract class AbstractMessage {
-    protected _messageSender: IMessageSender;
-    public abstract SendMessage(Message: string): string;
-}
--- notifications/libs/domain/src/imessage-sender.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IMessageSender {
-  SendMessage(Message: string): string
-}
